4263f2322b59dd2e68ffc0a19fcd716e2a974716
nicholas koyi <koyinicholas@gmail.com>
2024-03-27 12:18:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    let words = tutorial.split(' ');
+    let capitalizedWords = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
+};
+
